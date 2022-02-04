cbe27932f213c4bd5a287035661abe0e6cba9822
Darby Johnson <darbyjjohnson1@gmail.com>
2022-02-04 13:06:05 -0500

    set up code just have to work on routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  //when call categories will get the connected products as well
+  Category.findAll({
+    include: [Product]
+  }).then(db_data => res.json(db_data)).catch(err => res.json(err))
+  // be sure to include its associated Products
+  
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
